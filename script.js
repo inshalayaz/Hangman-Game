44e6ec6cfc3929dd56078116fae6f5281dc0631d
Inshal Ayaz <inshal.ayaz15@gmail.com>
2020-08-19 20:46:31 +0500

    display word function and notification feature added

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ const figureParts = document.querySelectorAll('.figure-part');
 
 const word = ['pokemon','programming','pikachu','virus','noob']
 
-const selectWord = word[Math.floor(Math.random() * word.length )]
+const selectedWord = word[Math.floor(Math.random() * word.length )]
 
 const correctLetters = []
 const wrongLetters = []
 
 function displayWord(){
     wordEl.innerHTML = `
-        ${selectWord
+        ${selectedWord
             .split('')
             .map(letter =>`
                 <span class="letter">
@@ -26,7 +26,48 @@ function displayWord(){
                 `
             ).join('')
         }
-    `
+        `
+        const innerWord = wordEl.innerText.replace(/\n/g,'')
+
+        if(innerWord === selectedWord){
+            finalMessage.innerHTML = 'Congratulations! you have Won!'
+            popup.style.display = 'flex'
+        }
+    }
+
+// Update Wrong Letters
+// function updateWrongLetterEl(){
+
+// }
+// Show Notification
+function showNotification(){
+    notification.classList.add('show')
+
+    setTimeout(()=>{
+        notification.classList.remove('show')
+    },2000)
 }
+// KEy Down event Listner
+
+window.addEventListener('keydown',e =>{
+    if(e.keyCode >=65 && e.keyCode <=90){
+        const letter = e.key
+        if(selectedWord.includes(letter)){
+            if(!correctLetters.includes(letter)){
+                correctLetters.push(letter)
+                displayWord()
+            }else{
+                showNotification()
+            }
+        }else{
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter)
+                updateWrongLettersEl()
+            }else{
+                showNotification()
+            }
+        }
+    }
+})
 
 displayWord()
